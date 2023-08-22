ff06cc3bc14b0c3f82c1860515f5f3c141718809
Can Sılacı <110562257+cansilaci1@users.noreply.github.com>
2023-08-22 22:53:41 +0300

    hafta1ArrayLists

---
+++ src/main/java/com/example/arraylist/ClassA.kt
@@ -0,0 +1,13 @@
+package com.example.arraylist
+
+class ClassA:Interfaces {
+    override val degisken: Int = 10
+
+    override fun metod1() {
+        println("inetrface merhaba")
+    }
+
+    override fun metod2(): String {
+        return "interface calismasi"
+    }
+}
---
+++ src/main/java/com/example/arraylist/Interfaces.kt
@@ -0,0 +1,13 @@
+package com.example.arraylist
+//interfaceleri hazır taslak olarak düşünebiliriz
+//interfaceler classlara özellik katmak için kullanılır
+//kalıtımda bir sınıfa sadece bir tane sınıfı kalıtım yoluyla aktarabiliyoruz
+//interface yapısında birden fazla interface ekleyebiliriz
+
+interface Interfaces {
+    val degisken:Int
+
+    fun metod1()
+
+    fun metod2():String
+}
